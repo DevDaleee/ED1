47f6832e42e21844a89cf06fbea42beee0f5a506
DevDalee <marcosdalessandroclima@ufpi.edu.br>
2022-12-12 13:33:44 -0300

    Tentativa de resolucao da questão

--- Codigos_Criado_Sala/main.c
+++ Codigos_Criado_Sala/main.c
@@ -8,40 +8,38 @@ que o usuário informou e métricas estatisticas como:
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-
-int media(int count, int *v){
-    float soma = 0;
+/*
+float media(int *v, int count){
+    float soma = 0, res;
     for (int i = 0; i < count; i++){
         soma += v[i];
     }
-    return soma/count;
+    res = soma/count;
+    return res;
 }
-/*
-double variancia(double s[], int n){
-    double soma = 0.0;
-    double dev = 0.0;
-    double med = media(n, s);
+
+int variancia(int *v, int count){
+    float soma = 0.0;
+    float dev = 0.0;
+    float med = media(count, v);
     int i = 0;
 
-    for( i = 0; i < n; i++ ){
-        dev = s[i] - med;
+    for( i = 0; i < count; i++ ){
+        dev = v[i] - med;
         soma += (dev * dev);
     }
 
-    return soma / n;
+    return soma / count;
 }
 
-double desvio_padrao (double s, int n){
-    double v = variancia(s, n);
-    return sqrt(v);
+int desvio_padrao (int *v, int count){
+    float res = variancia(v, count);
+    return sqrt(res);
 }
 */
-
 int main(){
     int *v, num = 0, total = 0, count = 0;
     
-
     v = (int *) malloc(1* sizeof(int));
 
     for (int i = 0; num != -1; i++){
@@ -53,6 +51,13 @@ int main(){
             v = (int *) realloc(v, sizeof(int) * count);
             v[i] = num;
         }
-        printf(media(count, v));
     }
+    for (int i = 0; i < count; i++){
+        printf(v[i]);
+    }
+    
+    //printf("Media  = %.2f \n",media(v, count));
+    //printf("Variancia = %f \n", variancia(v, count));
+    //printf("Desvio Padrão = %lf\n", desvio_padrao(v, count));
+    free(v);
 }
